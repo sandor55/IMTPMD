6f29b896fde7af7f66d83fb6e7a10285cfd7d94e
sandor55 <sandor_bakker1993@hotmail.com>
2016-05-26 13:04:31 +0200

    vakinfo en layout aangepast en apk aangemaakt.

--- app/src/main/java/com/imtpmd/sandor/imtpmdws/VakInfo.java
+++ app/src/main/java/com/imtpmd/sandor/imtpmdws/VakInfo.java
@@ -5,9 +5,15 @@ import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -18,7 +24,7 @@ import com.imtpmd.sandor.imtpmdws.Database.DatabaseInfo;
 /**
  * Created by sandor on 22-3-2016.
  */
-public class VakInfo extends AppCompatActivity {
+public class VakInfo extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
     //tags voor de data
     //database data
     String[] projection = {DatabaseInfo.CourseColumn.NAME, DatabaseInfo.CourseColumn.ECTS,DatabaseInfo.CourseColumn.GRADE, DatabaseInfo.CourseColumn.PERIOD};
@@ -41,6 +47,15 @@ public class VakInfo extends AppCompatActivity {
         setContentView(R.layout.vak_info);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
 
         //krijg de data
         Bundle bundle = getIntent().getExtras();
@@ -110,5 +125,60 @@ public class VakInfo extends AppCompatActivity {
          //update de tabel waar name gelijk is aan de name in de tabelnaam
         String[] args = {name};
        db.update(DatabaseInfo.CourseTables.COURSE,values,DatabaseInfo.CourseColumn.NAME + "=?" ,args);
+    }
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            Intent intent = new Intent (VakInfo.this, Settings.class);
+            startActivity(intent);
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_p1) {
+            startActivity(new Intent(this, MainActivity.class));
+            finish();
+
+        } else if (id == R.id.nav_p2) {
+            startActivity(new Intent(this, Invoer.class));
+            finish();
+
+        } else if (id == R.id.nav_p3) {
+            startActivity(new Intent(this, Overzicht.class));
+            finish();
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
